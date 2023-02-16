32cf0db073b0c34f3e06e2a7d772080855940b9c
ycm7784 <ycm7784@naver.com>
2023-02-16 18:02:02 +0900

    0216

--- edu/src/Chap5_recursive/eightQueen.java
+++ edu/src/Chap5_recursive/eightQueen.java
@@ -1,21 +1,30 @@
 package Chap5_recursive;
 
+import java.util.Stack;
+
 public class eightQueen {
-public static void SolveQueen(int[][] d) {
-	  for(int i = 0; i<=7;i++) {
-		  for(int j = 0;j<= 7 ; j++) {
-			  d[i][j]=0;
-			  System.out.print(d[i][j]);
-		  }
-		  System.out.println();
-	  }
+public static void SolveQueen(int[][] d, int a, int b) {
+	 
+	Stack stack = new Stack<>();
+//	d[a,b]=1;
+//	stack.push(d[a,b]);
 		
 	}
+public int checkRow(int x) {
+	
+	return 0;
+}
 	public static void main(String[] args) {
 		int [][]data = new int[8][8];  //배열초기화
-		SolveQueen(data);
-		
-	}
+		SolveQueen(data,0,0);
+		for(int i = 0; i<=7;i++) {
+			  for(int j = 0;j<= 7 ; j++) {
+				  data[i][j]=0;
+				  System.out.print(data[i][j]);
+			  }
+			  System.out.println();
+			}
+		}
 
 	
 
